5bb23fd148e8fe4d0a5db7ace22e7172c47d8de2
Michael Heimes <michael@cairndigitalmedia.com>
2020-05-23 21:33:09 -0400

    Covid Hero

--- front-page.php
+++ front-page.php
@@ -52,14 +52,32 @@ get_header(); ?>
 						
 							<div class="multi-color-line"><span class="color-1"></span><span class="color-2"></span><span class="color-3"></span><span class="color-4"></span><span class="color-5"></span></div>
 							
-							<div class="hashtag">#unitekindness</div>
+							<div class="hashtag"><?php the_field('temporary_heading');?></div>
 							
-							<p>Unite Kindness inspires children and teens to make a positive impact by being kind to themselves and others during COVID-19.</p>
+							<?php the_field('temporary_copy');?>
 							
 						</div>
 						
 						<div class="right">
-							<a class="button button-clear" href="/where-we-work/">Join The Campaign</a>
+							<?php 
+							$link = get_field('temporary_button_1');
+							if( $link ): 
+							    $link_url = $link['url'];
+							    $link_title = $link['title'];
+							    $link_target = $link['target'] ? $link['target'] : '_self';
+							    ?>
+							    <a class="button button-clear" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+							<?php endif; ?>
+							<?php 
+							$link = get_field('temporary_button_2');
+							if( $link ): 
+							    $link_url = $link['url'];
+							    $link_title = $link['title'];
+							    $link_target = $link['target'] ? $link['target'] : '_self';
+							    ?>
+							    <a class="button button-clear" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+							<?php endif; ?>
+
 						</div>	
 					
 					</div>				
